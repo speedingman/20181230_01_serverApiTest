e24cc0b867f9c9732ef71d04af52737fddc6934b
speedingman <hcdo@andats.com>
2018-12-30 11:22:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/zeros/a20181230_01_serverapitest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.zeros.a20181230_01_serverapitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
